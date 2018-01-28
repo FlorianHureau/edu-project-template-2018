f3e0d94665c930bab5a0e7e8dffed90580b331d5
Oworks <o_works@outlook.com>
2018-01-28 22:51:34 +0100

    Updating get all episode routing method and adding delete an episode, get an episode by his id routing methods

--- src/server/api.js
+++ src/server/api.js
@@ -7,10 +7,8 @@ const config = require('./config.js');
 const FindFiles = require('node-find-files');
 
 router.use(bodyParser.json());
-// router.get('/', function (req, res) {
-//     res.send('Coucou');
-// })
 
+//add an episode
 router.post('/', function (req, res) {
   var episode = req.body;
   episode.name = req.body.name;
@@ -22,11 +20,21 @@ router.post('/', function (req, res) {
 		res.sendStatus(500);
 	});
 	res.status(201).send(JSON.stringify({id : episode.id}));
-})
+});
 
+//Get an episode by is id
+router.get('/:idEp', function(req, res){
+  var idEp = req.params.idEp;
+  var path = config.data + "/" + idEp + ".json";
+  if(fs.existsSync(path)){
+    return res.json(JSON.parse(fs.readFileSync(path, 'utf8')));
+  }else {
+			return res.sendStatus(404);
+		}
+});
+
+//Get all episodes
 router.get('/', function(req, res){
-  console.log("coucou");
-  console.log(config.data);
   var finder = new FindFiles({
     rootFolder : config.data,
     filterFunction: (path) => {
@@ -36,19 +44,49 @@ router.get('/', function(req, res){
   });
   var files = [];
   finder.on('match', function(strPath, stat) {
-    console.log(strPath);
 		files.push(strPath);
 	});
   finder.on('complete', function() {
 		if(files.length==0) {
 			return res.sendStatus(204);
+		}	else {
+			var list = [];
+			var i = 0;
+			for(i; i < files.length ; i++)
+			{
+				var obj = JSON.parse(fs.readFileSync(files[i], 'utf8'));
+				list.push(obj);
+			}
+			return res.json(list);
 		}
-    console.log(files);
 	});
   finder.on('error', function(err) {
       console.log(err);
   });
   finder.startSearch();
+});
 
-})
+//Delete an episode by is id
+router.delete('/:idEp', function(req,res){
+  var idEp = req.params.idEp;
+  var path = config.data + "/" + idEp + ".json";
+  if(fs.existsSync(path)){
+    fs.unlinkSync(path);
+		return res.sendStatus(204);
+  }else {
+    console.log(req.data);
+		return res.sendStatus(404);
+	}
+});
+
+//Update
+// router.put('/:idEp', function(req,res){
+//   var idEp = req.params.idEp;
+//   var path = config.data + "/" + idEp + ".json";
+//   if(fs.existsSync(path)){
+//     console.log('coucou');
+//   }else {
+// 			return res.sendStatus(404);
+// 		}
+// }
 module.exports = router
